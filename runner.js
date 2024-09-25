d188d879e034583c2f15bcad03bd0376dfdd56d4
Pankaj Aswal <aswal.pankaj123@gmail.com>
2024-09-25 19:41:27 +0530

    Add files via upload

---
+++ runner.js
@@ -0,0 +1,17 @@
+﻿
+function NewWindow() {
+	windowscreate = chrome.windows.create({
+		height:600,
+		width:800,
+		type:"popup",
+		url:chrome.runtime.getURL("Main.html")
+    });
+}
+chrome.browserAction.onClicked.addListener(NewWindow);	
+
+function handleInstalled(details) {
+    browser.tabs.create({
+        url: "https://electricalcodes.blogspot.com"
+    });
+}
+//browser.runtime.onInstalled.addListener(handleInstalled);
